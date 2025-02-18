762a3610117fa5ffead8fc017f8486dff224078b
Xiaobai2-2022 <z2367li@uwaterloo.ca>
2025-02-18 14:34:33 +0800

    Added color support for log.h

--- include/util/log.h
+++ include/util/log.h
@@ -6,7 +6,9 @@
 #define LOG_H
 
 // Base case
-void inline print_args(){}
+void inline print_args() {
+    std::cout << "\033[0m" << std::endl;
+}
 
 template <typename T, typename ... Args>
 void print_args(T first, Args... args) {
@@ -14,63 +16,58 @@ void print_args(T first, Args... args) {
     print_args(args...);
 }
 
-#if defined(DEBUG)
+#ifdef DEBUG
 
 #define LOG_DEBUG( ... ) \
-std::cout << "[DEBUG]:\t"; \
-print_args(__VA_ARGS__); \
-std::cout << std::endl;
+std::cout << "\033[36m[DEBUG]:\t"; \
+print_args(__VA_ARGS__);
 
 #define LOG_WARN( ... ) \
-std::cout << "[WARNING]:\t"; \
-print_args(__VA_ARGS__); \
-std::cout << std::endl;
+std::cout << "\033[31m[WARNING]:\t"; \
+print_args(__VA_ARGS__);
 
 #define LOG_ERROR( ... ) \
-std::cout << "[ERROR]:\t"; \
-print_args(__VA_ARGS__); \
-std::cout << std::endl;
+std::cout << "\033[33m[ERROR]:\t"; \
+print_args(__VA_ARGS__);
 
 #define LOG_INFO( ... ) \
-std::cout << "[INFO]:\t\t"; \
-print_args(__VA_ARGS__); \
-std::cout << std::endl;
+std::cout << "\033[32m[INFO]:\t\t"; \
+print_args(__VA_ARGS__);
 
-#elif defined(DEV)
+#endif
+
+#ifdef DEV
 
 #define LOG_DEBUG( ... )
 
 #define LOG_WARN( ... ) \
-std::cout << "[WARNING]:\t"; \
-print_args(__VA_ARGS__); \
-std::cout << std::endl;
+std::cout << "\033[31m[WARNING]:\t"; \
+print_args(__VA_ARGS__);
 
 #define LOG_ERROR( ... ) \
-std::cout << "[ERROR]:\t"; \
-print_args(__VA_ARGS__); \
-std::cout << std::endl;
+std::cout << "\033[33m[ERROR]:\t"; \
+print_args(__VA_ARGS__);
 
 #define LOG_INFO( ... ) \
-std::cout << "[INFO]:\t\t"; \
-print_args(__VA_ARGS__); \
-std::cout << std::endl;
+std::cout << "\033[32m[INFO]:\t\t"; \
+print_args(__VA_ARGS__);
+
+#endif
 
 
-#elif defined(PROD)
+#ifdef PROD
 
 #define LOG_DEBUG( ... )
 
 #define LOG_WARN( ... )
 
 #define LOG_ERROR( ... ) \
-std::cout << "[ERROR]:\t"; \
-print_args(__VA_ARGS__); \
-std::cout << std::endl;
+std::cout << "\033[33m[ERROR]:\t"; \
+print_args(__VA_ARGS__);
 
 #define LOG_INFO( ... ) \
-std::cout << "[INFO]:\t\t"; \
-print_args(__VA_ARGS__); \
-std::cout << std::endl;
+std::cout << "\033[32m[INFO]:\t\t"; \
+print_args(__VA_ARGS__);
 
 #endif
 

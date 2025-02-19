482a1622fbeeccf57ac8ca2380d0c4d06d4ee20d
Xiaobai2-2022 <z2367li@uwaterloo.ca>
2025-02-19 14:31:43 +0800

    Created base class parsed_object and it's tests

---
+++ inc/main.h
@@ -0,0 +1,13 @@
+//
+// Created by xiaobai2-2025 on 2/18/25.
+//
+
+#pragma once
+
+#include <iostream>
+
+#include "debug_level.h"
+
+#include "util/log.h"
+
+#include "entity/parsed_object.h"
---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+//
+// Created by xiaobai2-2025 on 2/18/25.
+//
+
+#include "../inc/main.h"
+
+int main() {
+
+    PRINT_DEBUG( "TEST DEBUG", 1, "TEST" );
+    PRINT_WARN( "TEST WARN", 2 );
+    PRINT_ERROR( "TEST ERROR", 3, "ERROR" );
+    PRINT_INFO( "TEST INFO", 4, "IN", "FO" );
+
+    Parsed_Object parsed_object{3, 4};
+
+    parsed_object.print();
+
+    parsed_object.set_col(5);
+    parsed_object.print();
+
+    PRINT_DEBUG( parsed_object.get_row() );
+
+    parsed_object.set_row(10);
+    parsed_object.print();
+
+    PRINT_WARN( parsed_object.get_col() );
+
+}

5f2414d7b7bdfb3d4492c8694f4f2daae488f386
Xiaobai2-2022 <z2367li@uwaterloo.ca>
2025-02-24 13:21:00 +0800

    Renamed token_symbol to token_operator

---
+++ inc/entity/parsed_object/lex_object/concrete_lex_object/token_operator.h
@@ -0,0 +1,24 @@
+//
+// Created by xiaobai2-2025 on 2/21/25.
+//
+
+#pragma once
+
+#include "token.h"
+
+class Token_Operator final : public Token {
+
+private:
+    QURIO_Operator _value;
+
+public:
+    Token_Operator( const unsigned long &, const unsigned long &,
+        const Parsed_Lex_Type &, const QURIO_Operator & ) noexcept;
+    ~Token_Operator() noexcept override = default;
+
+public:
+    [[nodiscard]] std::any get_value() const noexcept override;
+
+    friend std::ostream & operator<<( std::ostream &, const Token_Operator & ) noexcept;
+
+}; // class Token_Operator <- Concrete
